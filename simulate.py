2446ab8863df2ffc35b5e2d9002805619530eb65
adamjovery <adamjovery2@gmail.com>
2026-09-22 14:15:48 +0100

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -186,7 +186,7 @@ while record != True:
 
 #Repeats until a valid confidence interval is entered
 while record2 != True:
-    print("\nPlease enter your desired confidence interval for the VaR and TVaR in decimal form, \ngreater than or equal to 1 and less than or equal to 0")
+    print("\nPlease enter your desired confidence interval for the VaR and TVaR in decimal form, \nwith this being less than or equal to 1 and greater than or equal to 0")
     error2 = 0
     confidence = input()
 
